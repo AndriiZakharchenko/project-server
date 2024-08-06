85431a4eb7740c9e78bc88a7b2fe9b8dfece1482
cicd_runner <cicd_runner@example.com>
2024-08-06 10:01:55 +0000

    Sync from branch module-5-network

---
+++ src/server.ts
@@ -0,0 +1,7 @@
+import http from 'http';
+// import { .... } from './error-handler';
+// import { .... } from './router';
+
+const PORT = process.env.PORT || 8000;
+
+// Implement the server using the http module to implement the API server with tasks what were described in the task description.
