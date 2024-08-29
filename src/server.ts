ebde8e372580be9f81736cbdb4dcd81234e0fbbf
dianababurina-photobox <diana.baburina@photobox.com>
2024-08-29 22:05:57 +0300

    clean up server template

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,5 @@
 import http from 'http';
-// import { .... } from './error-handler';
-// import { .... } from './router';
 
 const PORT = process.env.PORT || 8000;
 
-// Implement the server using the http module to implement the API server with tasks what were described in the task description.
+// Create and run server using http module
