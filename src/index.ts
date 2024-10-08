3d8378164584b83254385541915bff1c4b47dd2c
Andrii Zakharchenko <andrii.zakharchenko@effem.com>
2024-10-08 17:04:45 +0200

    module-3-eventemitter-buffer-streams

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-import { exportCsvToTxt } from './task-3-export-csv-to-txt';
-
-export { EventEmitter } from './task-1-event-emitter';
-export { WithTime } from './task-2-with-time';
-
-exportCsvToTxt('src/assets/books.csv', 'src/assets/books.txt').then(() => {});
+export * from './task-1-event-emitter';
+export * from './task-2-with-time';
+export * from './task-3-export-csv-to-txt';
